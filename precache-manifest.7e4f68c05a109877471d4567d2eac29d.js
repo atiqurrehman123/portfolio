dc3e37e533651cab962029ff48443e0707330cf3
aitq <aitu555@gmail.com>
2022-02-24 01:03:38 +0500

    Updates

---
+++ precache-manifest.7e4f68c05a109877471d4567d2eac29d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fa3a9fd504bf30a8d85c82f29fab63",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "27c66e29edcb9e681b75",
+    "url": "/portfolio/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "51359c1beb4e935f5f65",
+    "url": "/portfolio/static/css/main.d715ac16.chunk.css"
+  },
+  {
+    "revision": "27c66e29edcb9e681b75",
+    "url": "/portfolio/static/js/2.ee619ca6.chunk.js"
+  },
+  {
+    "revision": "7c2bd0664b0714c43d4fa05f933fc9a3",
+    "url": "/portfolio/static/js/2.ee619ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51359c1beb4e935f5f65",
+    "url": "/portfolio/static/js/main.f2de4d2d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c793c56e7cdb4c2015d1b9c0ac2d4791",
+    "url": "/portfolio/static/media/profilepic.c793c56e.jpeg"
+  }
+]);
